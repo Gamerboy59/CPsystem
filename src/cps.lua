080aab6920e2dfb33f0cfdbbfe2f2eb6800f1da2
Gamerboy59 <dev@gamerboy59.blue>
2015-11-16 22:48:27 +0100

    Some playing
    Added colors and setup a prefixtag.

--- src/cps.lua
+++ src/cps.lua
@@ -1,3 +1,4 @@
+prefixTag = cChatColor.Yellow..'[CPS]'
 function HandleCPSHelpCommand(Split, Player)
 	if( #Split <= 2 ) and Player:HasPermission("cps.help") then
 		Player:SendMessageInfo("------------ [Craft-Planet System] ------------")
@@ -6,6 +7,24 @@ function HandleCPSHelpCommand(Split, Player)
 		Player:SendMessageInfo("Usage: "..Split[1].." party <invite|kick|status|members> [playername]")
 		Player:SendMessageInfo("Usage: "..Split[1].." <show|set|give> [playername] [amount]")
 		Player:SendMessageInfo("Usage: "..Split[1].." <show|set|give> [playername] [amount]")
+	elseif Split[2] == "friend" and Player:HasPermission("cps.friend") then
+		Player:SendMessagePrivateMsg("Testing")
+	else
+		Player:SendMessageFailure("Use /help")
+	end
+	-- Player:SendMessageSuccess("Item repaired")
+	-- Player:SendMessageFailure("Please hold an item to repair")
+	return true
+end
+
+function HandleCPSCommand(Split, Player)
+	if( #Split <= 2 ) and Player:HasPermission("cps.global") then
+		Player:SendMessageInfo(cChatColor.LightGreen..'------------'..cChatColor.Green..' ['..cChatColor.Yellow..'Craft-Planet System'..cChatColor.Green..'] '..cChatColor.LightGreen..'------------')
+		Player:SendMessage(prefixTag..'This is our own and unique Server plugin.')
+		Player:SendMessage(prefixTag..cChatColor.LightGreen..' We created it for your ultimativ Minecraft Experience')
+		Player:SendMessage(prefixTag..cChatColor.LightGreen..' without wasting resources.')
+		Player:SendMessage(prefixTag..cChatColor.LightBlue..' This server is online 24/7.')
+		Player:SendMessage(prefixTag..cChatColor.Red..' Read the rules.')
 	elseif Split[2] == "friend" and Player:HasPermission("cps.friend") then
 		Player:SendMessagePrivateMsg("Testing")
 	else
